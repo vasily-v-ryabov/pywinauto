3de951e6e3f8cb3fbd08e405fee5b0e744e9194d
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2022-08-11 20:59:08 +0300

    Test https://github.com/enthought/comtypes/pull/338

--- ci/install.ps1
+++ ci/install.ps1
@@ -86,7 +86,7 @@ function UpdateConda ($python_home) {
 
 function InstallComtypes ($python_home) {
     $pip_path = $python_home + "\Scripts\pip.exe"
-    $args = "install https://github.com/bennyrowland/comtypes/archive/refs/heads/333_optin_numpy.zip"
+    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/avoid_SyntaxError_in_docstring.zip"
     Start-Process -FilePath "$pip_path" -ArgumentList $args -Wait -Passthru
 }
 
