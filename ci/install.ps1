8d38002c755ad8d2d22403f769a816c98c639343
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2022-09-20 09:53:00 +0300

    Test https://github.com/enthought/comtypes/pull/352

--- ci/install.ps1
+++ ci/install.ps1
@@ -86,7 +86,7 @@ function UpdateConda ($python_home) {
 
 function InstallComtypes ($python_home) {
     $pip_path = $python_home + "\Scripts\pip.exe"
-    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/fix_Array_import_src.zip"
+    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/add_typenamer.zip"
     Start-Process -FilePath "$pip_path" -ArgumentList $args -Wait -Passthru
 }
 
