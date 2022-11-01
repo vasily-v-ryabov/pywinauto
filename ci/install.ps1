178c1df581899c649e854580a4585febc780d633
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2022-11-01 17:52:29 +0300

    Test https://github.com/enthought/comtypes/pull/352

--- ci/install.ps1
+++ ci/install.ps1
@@ -86,7 +86,7 @@ function UpdateConda ($python_home) {
 
 function InstallComtypes ($python_home) {
     $pip_path = $python_home + "\Scripts\pip.exe"
-    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/remove_need_GUID.zip"
+    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/add_typenamer.zip"
     Start-Process -FilePath "$pip_path" -ArgumentList $args -Wait -Passthru
 }
 
