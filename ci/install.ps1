516b0eb218e6bb556685e20896cb43c670843b7a
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2022-11-01 17:42:14 +0300

    Test https://github.com/enthought/comtypes/pull/357

--- ci/install.ps1
+++ ci/install.ps1
@@ -86,7 +86,7 @@ function UpdateConda ($python_home) {
 
 function InstallComtypes ($python_home) {
     $pip_path = $python_home + "\Scripts\pip.exe"
-    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/remove_monkeypatch_COMError.zip"
+    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/remove_need_GUID.zip"
     Start-Process -FilePath "$pip_path" -ArgumentList $args -Wait -Passthru
 }
 
