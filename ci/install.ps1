97492bbcd60088f6802507a46e753a4742b94c03
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2022-11-02 15:01:21 +0300

    Test https://github.com/enthought/comtypes/pull/360

--- ci/install.ps1
+++ ci/install.ps1
@@ -86,7 +86,7 @@ function UpdateConda ($python_home) {
 
 function InstallComtypes ($python_home) {
     $pip_path = $python_home + "\Scripts\pip.exe"
-    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/add_typenamer.zip"
+    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/known_symbols_attributes.zip"
     Start-Process -FilePath "$pip_path" -ArgumentList $args -Wait -Passthru
 }
 
