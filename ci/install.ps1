5058ac59863ee8aa46a5f18da2e57c9ecd18e88b
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2022-11-03 09:11:46 +0300

    Test https://github.com/enthought/comtypes/pull/365

--- ci/install.ps1
+++ ci/install.ps1
@@ -86,7 +86,7 @@ function UpdateConda ($python_home) {
 
 function InstallComtypes ($python_home) {
     $pip_path = $python_home + "\Scripts\pip.exe"
-    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/first_type_hints.zip"
+    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/top_level_fix_inout_args.zip"
     Start-Process -FilePath "$pip_path" -ArgumentList $args -Wait -Passthru
 }
 
