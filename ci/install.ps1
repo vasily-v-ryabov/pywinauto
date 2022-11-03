2665e9a75c1769c1559e750172d5a47db8802f05
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2022-11-03 09:55:14 +0300

    Test https://github.com/enthought/comtypes/pull/367

--- ci/install.ps1
+++ ci/install.ps1
@@ -86,7 +86,7 @@ function UpdateConda ($python_home) {
 
 function InstallComtypes ($python_home) {
     $pip_path = $python_home + "\Scripts\pip.exe"
-    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/fix_docstring_from_DispInterfaceHead.zip"
+    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/add_memberspecs.zip"
     Start-Process -FilePath "$pip_path" -ArgumentList $args -Wait -Passthru
 }
 
